86aae238a191a5134a6dd990d0141f9155157766
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2025-04-10 23:22:03 -0400

    Create ServerTaskJson.kt

---
+++ ServerTaskJson.kt
@@ -0,0 +1,26 @@
+package com.example.project6
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ServerTaskJson {
+    suspend fun fetchJson(urlString: String): String = withContext(Dispatchers.IO) {
+        try {
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = reader.use { it.readText() }
+            connection.disconnect()
+            response
+        } catch (e: Exception) {
+            e.printStackTrace()
+            ""
+        }
+    }
+}
